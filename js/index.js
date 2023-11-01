54e1dc2226d9ecb206fc16cb2922c89df2b59c2e
My-code <mohammadshadhin01@gmail.comm>
2023-11-01 11:03:13 +0600

    final push

--- js/index.js
+++ js/index.js
@@ -1,2 +1,6 @@
-const elementToRemove = document.getElementById('myElement');
-elementToRemove.remove();
+
+document.getElementById('delete-btn').addEventListener('click', function(){
+    const imageRemove = document.getElementById('img-d');
+         imageRemove.remove();
+})
+
